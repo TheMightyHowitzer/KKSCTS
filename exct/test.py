90bd1d4919d2d829640486efdfd42ada7d17d3f3
Dau4096 <mrdau4096@gmail.com>
2025-10-25 18:04:43 +0100

    New values

--- exct/test.py
+++ exct/test.py
@@ -34,16 +34,89 @@ with open(filePath, "rb") as pngFile:
 		data.extend(ln);
 
 
-searchInt:list[int] = [ord(c) for c in "bustWeight"];
-index:int = searchInList(data, searchInt);
-relevantData:list[int] = data[index:];
+
 
 caIndex:int = searchInList(data, [0xCA,]);
-floatBytes:list[int] = relevantData[caIndex:caIndex+4];
+acIndex:int = searchInList(data, [0xAC,]);
+relevantData:list[int] = data[caIndex:acIndex];
+numValues:int = len(relevantData) // 5;
 
-rawFloat:float = translator.bytesToFloat(bytes(floatBytes));
-cleanFloat:int = int(round(rawFloat, 2) * 100.0);
-print(cleanFloat)
+
+valueMap:dict[int, str] = {
+	24: "Body Height",
+
+	28: "Breast Size",
+
+	33: "Breast Depth",
+	34: "Breast Roundness",
+
+	38: "Shoulder Width",
+	39: "Shoulder Thickness",
+	40: "Upper Torso Width",
+	41: "Upper Torso Thickness",
+	42: "Lower Torso Width",
+	43: "Lower Torso Thickness",
+
+	45: "Belly Thickness",
+	46: "Waist Width",
+	47: "Waist Thickness",
+	48: "Hip Width",
+	49: "Hip Thickness",
+	50: "Butt Size",
+	
+	52: "Upper Thigh Width",
+	53: "Upper Thigh Thickness"
+};
+fileData:str = "";
+
+
+
+LOWEST_ALLOWABLE_VALUE:float = 1.0e-5;
+HIGHEST_ALLOWABLE_VALUE:float = 2.1;
+WHITESPACE:int = 32;
+OUTPUT_FILE:str = "out.txt";
+
+
+
+for i in range(numValues):
+	if (i not in valueMap): continue;
+
+	bytesData:bytes = bytes(relevantData[(i*5):((i+1)*5-1)]);
+	rawFloatValue:float = translator.bytesToFloat(bytesData);
+	if ((rawFloatValue > HIGHEST_ALLOWABLE_VALUE) or (abs(rawFloatValue) < LOWEST_ALLOWABLE_VALUE)): continue;
+
+	cleanedFloatValue:int = int(round(rawFloatValue, 2) * 100.0);
+	search:str = valueMap[i];
+	fileData += (f"Search: {search}{(WHITESPACE-len(search))*' '}| Value: {cleanedFloatValue}\n");
+
+
+
+
+
+
+searches:list[str] = {
+	"bustSoftness",
+	"bustWeight",
+	"areolaSize"
+};
+
+
+for search in searches:
+	searchInt:list[int] = [ord(c) for c in search];
+	index:int = searchInList(data, searchInt);
+	relevantData:list[int] = data[index:];
+
+	caIndex:int = searchInList(data, [0xCA,]);
+	floatBytes:list[int] = relevantData[caIndex:caIndex+4];
+
+	rawFloat:float = translator.bytesToFloat(bytes(floatBytes));
+	cleanFloat:int = int(round(rawFloat, 2) * 100.0);
+	fileData += (f"Search: {search}{(WHITESPACE-len(search))*' '}| Value: {cleanFloat}\n");
+
+
+
+with open(OUTPUT_FILE, "w") as outFile:
+	outFile.write(fileData);
 
 
 
